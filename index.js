503b0130ba3f1bb9f7ac6da0b615d7c563b84327
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2016-04-13 16:18:49 -0700

    smaller cleanups

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var express = require('express')
 var app = express()
-app.use(express.static(__dirname + '/public'));
 var bodyParser = require('body-parser')
-  var pg = require('pg');
+var pg = require('pg');
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000))
 
-  app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname+'/index.html'));
-  });
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.post('/alerts/:id',function(req,res){
   var id = req.params.id;
@@ -21,21 +21,17 @@ app.post('/alerts/:id',function(req,res){
       console.log(err);
       if (err) return res.send(500);
       if (result.rows.length == 0) return res.json(result.rows);
-
       result.rows.map(function(row){
         try {
           row.data = JSON.parse(row.data);
         } catch (e) {
           row.data = null;
         }
-
         return row;
       });
-
       return response.json(result.rows);
     });
   });
-
 });
 
 app.get('/alerts', function (request, response) {
@@ -44,7 +40,6 @@ app.get('/alerts', function (request, response) {
     client.query('SELECT * FROM alerts', function(err, result) {
       done();
       if (err) return response.send(500);
-
       if (result.rows.length == 0) return response.json(result.rows);
       result.rows.map(function(row){
         try {
@@ -52,7 +47,6 @@ app.get('/alerts', function (request, response) {
         } catch (e) {
           row.data = null;
         }
-
         return row;
       });
 
@@ -60,6 +54,7 @@ app.get('/alerts', function (request, response) {
     });
   });
 })
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
